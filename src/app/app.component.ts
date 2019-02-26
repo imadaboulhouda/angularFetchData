5351844e3c2842ebad64aeb828911b340b4553a3
IMAD ABOULHOUDA <imadaboulhouda1@gmail.com>
2019-02-26 10:13:03 +0100

    Just for fetch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { DataServiceService } from './data-service.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  data = [];
   title = 'just';
+  constructor(private service: DataServiceService)
+  {
+    this.service.getData().subscribe(e=>{
+        for(let x in e)
+        {
+          this.data.push(e[x].name);
+        }
+    });
+  }
 }
